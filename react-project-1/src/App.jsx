fe5f4d564d4cdc6bac14cb6bbc8694be253e8801
Himanshu Tripathi <himanshuramtripathi10@gmail.com>
2026-03-31 21:14:05 +0530

    Components started

--- react-project-1/src/App.jsx
+++ react-project-1/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+// import Header from './Components'
 
    const headerContent = ["Getting Start","Lets Start","Get Started"];
    function setRandomNumber(max){
@@ -13,28 +14,26 @@ function App() {
    let dynamicContent = headerContent[setRandomNumber(headerContent.length-1)];
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>{dynamicContent}</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
+       <section id="center">
+               <div className="hero">
+                 <img src={heroImg} className="base" width="170" height="179" alt="" />
+                 <img src={reactLogo} className="framework" alt="React logo" />
+                 <img src={viteLogo} className="vite" alt="Vite logo" />
+               </div>
+               <div>
+                 <h1>{dynamicContent}</h1>
+                 <p>
+                   Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+                 </p>
+               </div>
+               <button
+                 className="counter"
+                 onClick={() => setCount((count) => count + 1)}
+               >
+                 Count is {count}
+               </button>
+             </section>
+       <div className="ticks"></div>
       <section id="next-steps">
         <div id="docs">
           <svg className="icon" role="presentation" aria-hidden="true">
