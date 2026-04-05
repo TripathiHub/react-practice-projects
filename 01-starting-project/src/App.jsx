0519c06dcc152f614d0af126118e5a003fa50fa8
Himanshu Tripathi <himanshuramtripathi10@gmail.com>
2026-04-06 00:22:37 +0530

    used map funtion to output list dynamically

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -20,15 +20,16 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
            <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-            {/*title={CORE_CONCEPTS[0].title}
+           {/* <CoreConcept {...CORE_CONCEPTS[0]}/>
+            title={CORE_CONCEPTS[0].title}
             description={CORE_CONCEPTS[0].description}
             image={CORE_CONCEPTS[0].image} 
             this format will also work in all cases but if you have same key value of imput onject
-            to props name then you can use spread opretor to optimize your code */}
+            to props name then you can use spread opretor to optimize your code 
             <CoreConcept {...CORE_CONCEPTS[1]}/>
             <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/> */}
+            {CORE_CONCEPTS.map((contentItem)=> <CoreConcept key={contentItem.title} {...contentItem}/>)}
            </ul>
         </section>
         <section id='examples'>
