e47e024c3f8ad8b9687667830dcf71c264311b32
Himanshu Tripathi <himanshuramtripathi10@gmail.com>
2026-06-04 10:32:48 +0530

    Investment caculator is changed to sip calculator so changes in investment.js done

--- 02-starting-project (1)/01-starting-project/src/components/UserInput.jsx
+++ 02-starting-project (1)/01-starting-project/src/components/UserInput.jsx
@@ -5,28 +5,26 @@ export default function UserInput({onChange,userInput}){
     <section id="user-input">
       <div className="input-group">
         <p>
-            <label>Intial Investment</label>
+            <label>Monthly SIP Amount</label>
             <input type="number" required value={userInput.initialInvestment}
             onChange={(event)=> onChange("initialInvestment",event.target.value)} />
         </p>
          <p>
-            <label>Annual Investment</label>
+            <label>Excepted Annual Return</label>
             <input type="number" required value={userInput.annualInvestment}
              onChange={(event)=> onChange("annualInvestment",event.target.value)}/>
         </p>
       </div>
       <div className="input-group">
         <p>
-            <label>Excepted Return</label>
+            <label>Duration</label>
             <input type="number" required value={userInput.expectedReturn}
              onChange={(event)=> onChange("expectedReturn",event.target.value)}/>
         </p>
-         <p>
-            <label>Duration</label>
-            <input type="number" required value={userInput.duration}
-             onChange={(event)=> onChange("duration",event.target.value)}/>
-        </p>
       </div>
+       <p>
+        <button color="blue">Calculate</button>
+       </p>
     </section>
           </>
 }
--- 02-starting-project (1)/01-starting-project/src/util/investment.js
+++ 02-starting-project (1)/01-starting-project/src/util/investment.js
@@ -1,38 +1,45 @@
-// This function expects a JS object as an argument
-// The object should contain the following properties
-// - initialInvestment: The initial investment amount
-// - annualInvestment: The amount invested every year
-// - expectedReturn: The expected (annual) rate of return
-// - duration: The investment duration (time frame)
 export function calculateInvestmentResults({
-  initialInvestment,
-  annualInvestment,
-  expectedReturn,
+  monthlySipAmount,
+  expectedAnnualReturn,
   duration,
 }) {
-  const annualData = [];
-  let investmentValue = initialInvestment;
-
-  for (let i = 0; i < duration; i++) {
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
-    annualData.push({
-      year: i + 1, // year identifier
-      interest: interestEarnedInYear, // the amount of interest earned in this year
-      valueEndOfYear: investmentValue, // investment value at end of year
-      annualInvestment: annualInvestment, // investment added in this year
-    });
-  }
-
-  return annualData;
+ const months=duration*12;
+ const monthlyRate = expectedAnnualReturn/12/100;
+ const totalInvested = months*monthlySipAmount;
+ const maturityValue = monthlySipAmount*
+ (((Math.pow(1+monthlyRate,months)-1)/monthlyRate)*(1+monthlyRate));
+ const wealthGained = maturityValue-totalInvested;
+return({
+  totalInvested,
+  wealthGained,
+  maturityValue
+})
 }
-
-// The browser-provided Intl API is used to prepare a formatter object
-// This object offers a "format()" method that can be used to format numbers as currency
-// Example Usage: formatter.format(1000) => yields "$1,000"
-export const formatter = new Intl.NumberFormat('en-US', {
+export function calculateYearlyData({
+  monthlySipAmount,
+  expectedAnnualReturn,
+  duration
+}){
+  const yearlyData = [];
+   for( let year=1;year<=duration;year++){
+    const months=year*12;
+    const monthlyRate = expectedAnnualReturn/12/100;
+    const totalInvested = monthlySipAmount*months;
+    const maturityValue = monthlySipAmount*
+    (((Math.pow(1+monthlyRate,months)-1)/monthlyRate)*(1+monthlyRate));
+    const wealthGained = maturityValue-totalInvested;
+    yearlyData.push({
+      year,
+      totalInvested,
+      wealthGained,
+      maturityValue
+    })
+   }
+   return yearlyData;
+}
+export const formatter = new Intl.NumberFormat('en-IN', {
   style: 'currency',
-  currency: 'USD',
+  currency: 'INR',
   minimumFractionDigits: 0,
   maximumFractionDigits: 0,
 });
